e1751045a651151a5ee64721e4800d54b93024a6
KhramtsovaV <khramtsova.valeria@gmail.com>
2024-05-19 22:40:24 +0300

    task QA27_4_1_Khramtsova

--- src/hw5/QA27_4_1_Khramtsova.java
+++ src/hw5/QA27_4_1_Khramtsova.java
@@ -1,4 +1,25 @@
 package hw5;
 
 public class QA27_4_1_Khramtsova {
+    public static void main(String[] args) {
+        char[][] board = new char[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+
+                if ((i + j) % 2 == 0) {
+                    board[i][j] = 'W';
+                } else {
+                    board[i][j] = 'B';
+                }
+            }
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
